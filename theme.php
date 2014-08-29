3cd886f2711630d1a287dcd923813a0de30bd01c
Holger Koenemann <hello@holgerkoenemann.de>
2014-08-29 09:36:56 +0200

    Adding files

---
+++ theme.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+        'positions' => [
+
+
+        'navbar'     => 'Navbar',
+        'sidebar'     => 'Sidebar',
+        'footer'     => 'Footer',
+        'hero'     => 'Hero',
+        'top'     => 'Top',
+        'bottom'     => 'Bottom',
+
+
+    ],
+
+    'renderer' => [
+
+
+        'blank'     => 'theme://shelly/views/renderer/position.blank.razr',
+        'grid'      => 'theme://shelly/views/renderer/position.grid.php',
+        'navbar'    => 'theme://shelly/views/renderer/position.navbar.razr',
+        'offcanvas' => 'theme://shelly/views/renderer/position.offcanvas.razr',
+        'panel'     => 'theme://shelly/views/renderer/position.panel.razr'
+
+
+    ]
+
+];
